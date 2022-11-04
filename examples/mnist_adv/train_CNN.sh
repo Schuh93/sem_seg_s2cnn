8c0336fdb6a7f7894be2377a490b01351da0239a
Daniel Schuh <schuh@hep.itp.tuwien.ac.at>
2022-11-04 22:01:01 +0100

    write train_CNN script

---
+++ examples/mnist_adv/train_CNN.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+export PYTHONWARNINGS="ignore"
+
+
+for train_samples in 10000 20000 30000 40000 50000 60000 80000 100000 120000 240000 600000
+do
+    for i in {1..3}
+    do
+      python train_CNN.py \
+                --name "adv_study" \
+                --channels 16 32 64 32 16 \
+                --kernels 3 3 3 3 3 \
+                --strides 1 1 1 1 1 \
+                --nodes 64 32 \
+                --train_samples $train_samples \
+                --activation_fn "LeakyReLU";
+    done
+done
+
+
+for train_samples in 10000 20000 30000 40000 50000 60000 80000 100000 120000 240000 600000
+do
+    for i in {1..3}
+    do
+      python train_CNN.py \
+                --name "adv_study" \
+                --channels 16 24 32 64 \
+                --kernels 3 3 3 3 3 \
+                --strides 1 1 1 1 1 \
+                --nodes 64 32 \
+                --train_samples $train_samples \
+                --activation_fn "LeakyReLU";
+    done
+done
+
+
+for train_samples in 10000 20000 30000 40000 50000 60000 80000 100000 120000 240000 600000
+do
+    for i in {1..3}
+    do
+      python train_CNN.py \
+                --name "adv_study" \
+                --channels 16 24 32 64 128 \
+                --kernels 3 3 3 3 3 \
+                --strides 1 1 1 1 1 \
+                --nodes 64 32 \
+                --train_samples $train_samples \
+                --activation_fn "LeakyReLU";
+    done
+done
+
+
+for train_samples in 10000 20000 30000 40000 50000 60000 80000 100000 120000 240000 600000
+do
+    for i in {1..3}
+    do
+      python train_CNN.py \
+                --name "adv_study" \
+                --channels 16 24 32 64 128 \
+                --kernels 3 5 3 5 3 \
+                --strides 1 2 1 2 1 \
+                --nodes 64 32 \
+                --train_samples $train_samples \
+                --activation_fn "LeakyReLU";
+    done
+done
+
+
+for train_samples in 10000 20000 30000 40000 50000 60000 80000 100000 120000 240000 600000
+do
+    for i in {1..3}
+    do
+      python train_CNN.py \
+                --name "adv_study" \
+                --channels 16 24 32 64 128 64 \
+                --kernels 3 5 5 5 3 3 \
+                --strides 1 2 1 2 1 1 \
+                --nodes 64 32 \
+                --train_samples $train_samples \
+                --activation_fn "LeakyReLU";
+    done
+done
