43b2bcb9801e24b580efb3711937ab74ec18d327
Daniel Schuh <schuh@hep.itp.tuwien.ac.at>
2023-11-15 12:29:23 +0100

    add DeepFool attack

--- examples/mnist_adv/baseline_PGD.sh
+++ examples/mnist_adv/baseline_PGD.sh
@@ -3,12 +3,9 @@ export PYTHONWARNINGS="ignore"
 
 
 
-for run_names in 1697665587 1697670845 1697684622
+for run_names in 1669374570
 do
 python baseline_PGD.py \
         --run_name $run_names \
-        --rel_stepsize 0.05 \
-        --steps 50 \
-        --epsilons 0 0.5 2.5 5 7.5 10 14 20 30 \
-        --bs 100
+        --bs 10
 done
